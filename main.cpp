5353220aae872eb3c15e685deaee254e88f278b4
Diego Caviedes <diegocaviedes219@gmail.com>
2022-11-24 12:19:55 -0500

    Commit Inicial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Diego Caviedes
+ * 1726325663
+ * dcaviedesa@est.ups.edu.ec
+ * 24/11/2022
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "====EXAMEN 1 PARCIAL POO====" << endl;
+cout << "Autor: Diego Caviedes\n";
+return 0;
+}
